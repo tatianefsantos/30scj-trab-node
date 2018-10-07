30ae63894beafb7ad0c6a96ed0b9956c9c9fe100
Tatiane dos Santos <tatianefsantos@gmail.com>
2018-10-06 22:00:35 -0300

    Ajustes

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var load = require('express-load');
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
+var error = require('./middlewares/error');
+
+var port = process.env.PORT || 3000;
+
+app = express();
+
+var mongoose = require('mongoose');
+global.db = mongoose.connect('mongodb://admin:admin1@ds151078.mlab.com:51078/ncursos');
+mongoose.connection.on('connected', function () {
+  console.log('=====Conexão estabelecida com sucesso=====');
+});
+mongoose.connection.on('error', function (err) {
+  console.log('=====Ocorreu um erro: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('=====Conexão finalizada=====');
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(cookieParser('ncursos'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app);
+
+//middlewares
+app.use(error.notFound);
+app.use(error.serverError);
+
+app.listen(port, function () {
+  console.log("Aplicação no ar.");
+});
